2f266f346aef5c06a05f5fd944af525fd326bcd9
kevin_desktop <nature2575@yeah.net>
2013-04-01 12:44:00 +0800

    Attempt: 参考图像与降质图像的差值评价

--- tumorselection/ui.m
+++ tumorselection/ui.m
@@ -710,32 +710,34 @@ ssimIndex = doubleInd2singleInd(curSlice,ssimMax,handles);
 %尝试对参考图像分10级进行模糊，比较其他图像与这10级中哪一级相似，也就确定了该图像的模糊程度
 imgRef =  double(dicomread(dcmInfo{curIndex}));
 imgBlurry(:,:,1) = imgRef;
-for i = 2:10
-    %filt = fspecial('motion',i*1.9,theta);
-    filt = fspecial('gaussian',3,(i-1)*0.07+0.3);
-    imgBlurry(:,:,i) = imfilter(imgRef,filt,'circular');
-end
 num = length(ssimIndex);
 for p = 1:num
     img = double(dicomread(dcmInfo{ssimIndex(p)}));
-    for i = 1:10
-        tmp = corrcoef(imgBlurry(:,:,i),img);
-        similarity(i) = tmp(2);
-    end
-    tmp = find(similarity == max(similarity));
-    blurDegree(p) = tmp(1);
+    tmp = imgRef - img;
+    con(p) = cmptContr(tmp);
+    def(p) = cmptDefn(tmp);
+    ent(p) = entropy(tmp);
+    sn(p) = snr(imgRef,img);
 end
-% 临时代码======================
-figure,plot(blurDegree,'-*'),hold on;
+figure,plot(con,'-*'),hold on;
 for i = 1:num
-    text(i,blurDegree(i),num2str(ssimMax(i)));
+    text(i,con(i),num2str(i));
 end
-% ======================
-tmp = blurDegree;
-tmp(tmp==min(tmp)) = [];
-t = min(tmp);
-ssimIndex = ssimIndex(blurDegree <= t);
-ssimMax = ssimMax(blurDegree <= t);
+figure,plot(def,'-*'),hold on;
+for i = 1:num
+    text(i,def(i),num2str(i));
+end
+figure,plot(ent,'-*'),hold on;
+for i = 1:num
+    text(i,ent(i),num2str(i));
+end
+figure,plot(sn,'-*'),hold on;
+for i = 1:num
+    text(i,sn(i),num2str(i));
+end
+
+% ssimIndex = ssimIndex(blurDegree <= t);
+% ssimMax = ssimMax(blurDegree <= t);
 %====================================
 for i=1:length(ssimIndex)
     img = double(dicomread(dcmInfo{ssimIndex(i)}));
